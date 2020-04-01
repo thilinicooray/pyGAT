a148c312c5c2f477f764c5ee8fc75d99201f3c18
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-01 13:38:21 +0800

    size check

--- models.py
+++ models.py
@@ -17,6 +17,9 @@ class GAT(nn.Module):
         self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
 
     def forward(self, x, adj):
+
+        print('infor ', x.size(), adj.size())
+
         x = F.dropout(x, self.dropout, training=self.training)
         x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
         x = F.dropout(x, self.dropout, training=self.training)
